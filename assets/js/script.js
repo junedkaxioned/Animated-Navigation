aa1ea524b439e256413f17047cc643486b8eade8
JKAX2022 <junedk@axioned.com>
2022-10-01 13:30:08 +0530

    Js Complleted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-/* Author: Zishan Ansari */
+var toggle = document.querySelector('.toggle');
+var navbar = document.querySelector('.nav');
+
+toggle.addEventListener('click', function () {
+  navbar.classList.toggle("active");
+})
